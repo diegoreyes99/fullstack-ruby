7c7be9717fbe075a12f20089c10ba6644aa8c8bb
diegoreyes99 <142082412+diegoreyes99@users.noreply.github.com>
2025-01-02 16:52:50 -0600

    Add files via upload

---
+++ 04-Front-end/09-Stimulus-JS/02-Copy-To-Clipboard/lib/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/extensions */
+import { Application } from "@hotwired/stimulus";
+
+// TODO: Import the Stimulus Controller here
+
+Stimulus = Application.start();
+// TODO: Register your Stimulus Controller below
