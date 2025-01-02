92b3bf6fe80042786aaf949361b9ee5baef896b1
Diego Reyes <reyess_diego@outlook.com>
2025-01-02 17:31:23 -0600

    Completed

--- 00-Setup/00-Demo/lib/demo.rb
+++ 00-Setup/00-Demo/lib/demo.rb
@@ -1,3 +1,12 @@
 def circle_area(radius)
   # TODO: Implement the `circle_area` method
+
+  if radius < 0
+    return 0
+  else
+    # Calcula el área del círculo con la fórmula correcta
+    area = 3.1416 * radius ** 2
+    return area
+  end
+
 end
